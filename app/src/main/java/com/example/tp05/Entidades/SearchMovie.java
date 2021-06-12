012a8b677c5fea88772d35f4d927a2ccd0e470af
MBelenPiva <83927735+MBelenPiva@users.noreply.github.com>
2021-06-12 18:22:03 -0300

    Proyecto en AndroidStudio con todas las carpetas

---
+++ app/src/main/java/com/example/tp05/Entidades/SearchMovie.java
@@ -0,0 +1,4 @@
+package com.example.tp05.Entidades;
+
+public class SearchMovie {
+}
---
+++ app/src/main/java/com/example/tp05/Utilities/LogHelper.java
@@ -0,0 +1,15 @@
+package com.example.tp05.Utilities;
+
+import android.util.Log;
+
+public class LogHelper {
+    public static final String LOG_TAG = "";
+
+    public static void d(String strMensaje){
+        Log.d(LOG_TAG, strMensaje);
+    }
+
+    public static void enArchivo(String message){
+
+    }
+}
